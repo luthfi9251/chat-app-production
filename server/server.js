a45aeb5ef749afc583a18a1ba32787c2cfb8e8a2
luthfi9251 <luthfi.irfan11@gmail.com>
2023-03-12 11:30:22 +0700

    change serverjs and package json

--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ app.get('/deletealluser',(req,res) => {
 //     console.log("Listening on port: " + PORT)
 // })
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client', 'dist')));
     app.get('/*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
@@ -97,4 +97,4 @@ app.listen(PORT, ()=> {
     console.log("Listening on port: " + PORT)
 })
 
-module.exports = app
+// module.exports = app
